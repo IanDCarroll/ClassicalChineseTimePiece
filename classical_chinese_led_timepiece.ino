eb59d4a9264461b872f2811a74ad6225f483c509
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-23 20:38:47 -0800

    add blinking lights for fen and miao

--- classical_chinese_led_timepiece.ino
+++ classical_chinese_led_timepiece.ino
@@ -1,22 +1,34 @@
-int one_miao = 144;
-int one_fen = 14400;
+int one_miao = 144; // milliseconds
+int one_fen = 100;  // miao
 
 void setup()
 {
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop()
 {
+  digitalWrite(9, HIGH);
+  digitalWrite(8, LOW);
+  delayOneFen();
+  digitalWrite(9, LOW);
+  digitalWrite(8, HIGH);
+  delayOneFen();
+}
+
+void delayOneFen()
+{
   int miao = 0;
   while(miao < 100){
-    miaoLoop();
+    blinkMiao();
     miao++;
   }
 }
 
-void miaoLoop()
+void blinkMiao()
 {
   digitalWrite(13, HIGH);
   digitalWrite(12, LOW);
